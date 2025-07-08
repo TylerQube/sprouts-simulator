d6cd2e8ec81b87c8e1da0b8de151b89c650e534b
TylerQube <tsmith22@students.cs.ubc.ca>
2025-07-07 20:11:27 -0700

    Add level transitions

--- LevelManager.gd
+++ LevelManager.gd
@@ -15,6 +15,7 @@ func set_levels(levels: Array[Node]):
 	self.levels = levels
 	
 func switch_level(new_level: Node):
+	print("switching level")
 	if current_level:
 		var ci = levels.find(current_level) + 1
 		_set_level_enabled(current_level, false, ci)
@@ -28,8 +29,22 @@ func switch_level(new_level: Node):
 	for i in range(1, len(levels)+1):
 		player.set_collision_mask_value(i, false)
 	player.set_collision_mask_value(li, true)
-
 	
+	
+	print(current_level)
+	var entry_point = current_level.get_node("EnterPoint").global_position
+	player.global_position = entry_point
+	
+	var point
+	if current_level.name == "KitchenLevel":
+		point = current_level.get_node("BreakfastPoint")
+	elif current_level.name == "CafeLevel":
+		point = current_level.get_node("RegisterPoint")
+	print(str(point))
+	player.agent.target_position = point.position
+	
+	
+	player.set_anim(Vector2.DOWN, false)
 	# new_level.visible = true
 	#_set_level_enabled(new_level, true)
 	
--- Player.gd
+++ Player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var speed = 70
+@export var speed = 60
 
 @export var left_tex: Texture
 @export var right_tex: Texture
@@ -14,6 +14,12 @@ func get_input():
 	else:
 		dir.x = 0
 	return dir
+	
+func get_cardinal(vec: Vector2):
+	if abs(vec.x) > abs(vec.y):
+		return Vector2.RIGHT if vec.x > 0 else Vector2.LEFT
+	else:
+		return Vector2.DOWN if vec.y > 0 else Vector2.UP
 
 func set_anim(dir: Vector2, moved: bool):
 	var anim = $AnimatedSprite2D
@@ -38,16 +44,29 @@ func set_anim(dir: Vector2, moved: bool):
 var old_pos : Vector2
 var pos : Vector2
 var tol = 1e-4
+
+var agent: NavigationAgent2D
 func _ready():
 	old_pos = position
-	pos = position
+	agent = $NavigationAgent2D
 
 func _physics_process(delta):
 	old_pos = position
-	var dir = get_input()
-	velocity = dir * speed
-	move_and_slide()
-	pos = position
-	set_anim(dir, (pos - old_pos).length_squared() > tol)
+	print()
+	print("currently at: " + str(position))
+	print("going to: " + str(self.agent.target_position))
+	print("next point: " + str(self.agent.get_next_path_position()))
+	var to_dest = self.agent.get_next_path_position() - old_pos
+	var dir = to_dest.normalized()
+	print("dir: " + str(dir))
+	
+	
+	if to_dest.length_squared() > 1:
+		velocity = dir * speed
+		move_and_slide()
+	
+
+	var moved = (position - old_pos).length_squared() > tol
+	set_anim(get_cardinal(dir), moved)
 	
 	
